e2a48b097060446695913150949893d4f7b6e168
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-12-05 05:08:11 +0700

    added role for user schemas

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import Roles from "../enums/roles";
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     tel: { type: String, required: true, unique: true },
+    role: { type: String, enum: Object.values(Roles), required: true, default: Roles.USER},
     authentication: {
         password: { type: String, required: true, select: false },
         salt: { type: String, select: false },
