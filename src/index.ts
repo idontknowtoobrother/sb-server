6c9c01709a5621b492d3858690746228be4517bb
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-12-05 03:24:25 +0700

    setup mongo, create user schema and user db service

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -19,6 +22,11 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log("Sever running on http://localhost:8080");
+server.listen(process.env.SERVER_PORT || 3000, () => {
+    console.log(`Sever running on port: ${process.env.SERVER_PORT || 3000}`);
 });
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error));
