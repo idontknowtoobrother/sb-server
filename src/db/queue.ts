9c3193cd813968360e3ca3c85aefff2359038efc
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-12-07 14:15:35 +0700

    added: queue schema

---
+++ src/db/queue.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const QueueSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    date: { type: Date, required: true },
+    picture: { type: String },
+    moreInformation: { type: String },
+    accepted: { type: Boolean, required: true, default: false }
+})
+
+export const QueueModel = mongoose.model('Queue', QueueSchema);
+
+export const getQueue = () => QueueModel.find();
+export const getQueueById = (id: string) => QueueModel.findById(id);
+export const getQueueByUserId = (userId: string) => QueueModel.findOne({ userId });
+export const createQueue = (values: Record<string, any>) => new QueueModel(values)
+    .save().then((queue) => queue.toObject());
+export const deleteQueueById = (id: string) => QueueModel.findOneAndDelete({ _id: id });
+export const updateQueueById = (id: string, values: Record<string, any>) => QueueModel.findByIdAndUpdate(id, values);
